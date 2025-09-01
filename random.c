41f07fe1bd6e13fa80ce705ff5d8456359b8528c
Ryan Staub <tup56209@temple.edu>
2025-08-31 23:00:58 -0400

    Finished rand_string and set size=10 in main!

--- random.c
+++ random.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <time.h>
+#include <stddef.h>  // for size_t
 
-char* rand_string(int length) {
-    static char charset[] = "abcdefghijklmnopqrstuvwxyz";
-    char *str = malloc(length + 1);
-    if (str) {
-        for (int i = 0; i < length; i++) {
-            int key = rand() % (int)(sizeof(charset) - 1);
-            str[i] = charset[key];
-        }
-        str[length] = '\0';
+// Fill `out` with random characters of length-1 and add '\0' at the end
+void rand_string(char *out, size_t length) {
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyz";
+    if (length == 0) return;
+
+    for (size_t i = 0; i + 1 < length; i++) {
+        int key = rand() % (sizeof charset - 1);
+        out[i] = charset[key];
     }
-    return str;
+    out[length - 1] = '\0';
 }
